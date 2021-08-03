8908e7573e69b020c067bd086d301e93c8e25bfe
Aditya Kumar <adityakumar@Adityas-MacBook-Pro.local>
2021-08-03 19:33:01 +0530

    adding programs

---
+++ exp1problem1.java
@@ -0,0 +1,16 @@
+import java.util.*;
+public class exp1problem1 {
+    public static void main(String[] args){
+        Scanner obj =new Scanner (System.in);
+        System.out.println("Enter number"); 
+        int a =obj.nextInt();
+        
+        System.out.println("Enter kth value"); 
+        int k =obj.nextInt();
+        for(int i=0;i<k-1;i++)
+        {
+            a=a/10;
+        }
+        System.out.println(a%10); 
+    }
+}
